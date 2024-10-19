72be5f5aa950a38bcee1d93082a6b2bf7857ddfb
dinarcoins <nhanhoang863@gmail.com>
2024-10-19 23:06:54 +0700

    state code

---
+++ js/constants.js
@@ -0,0 +1,144 @@
+export var navMenuList = [
+  {
+    text: "iec - hanu",
+    href: "#",
+  },
+  {
+    text: "tuyển sinh",
+    href: "#",
+  },
+  {
+    text: "chương trình đào tạo",
+    href: "#",
+  },
+  {
+    text: "tư vấn du học",
+    href: "#",
+  },
+  {
+    text: "cơ sở vật chất",
+    href: "#",
+  },
+];
+
+export var programItemList = [
+  {
+    img: "./imgs/program1.png",
+    title: "Chương trình cử nhân quốc tế",
+    text1: "Đại học IMC KREMS (Áo)",
+    text: "Đại học Waikato (New Zealand)",
+    marginTop: 0,
+  },
+  {
+    img: "./imgs/program2.png",
+    title: "Chương trình cử nhân quốc tế",
+    text1: "Đại học IMC KREMS (Áo)",
+    text: "Đại học Waikato (New Zealand)",
+    marginTop: 1,
+  },
+  {
+    img: "./imgs/program3.png",
+    title: "Chương trình cử nhân quốc tế",
+    text1: "Đại học IMC KREMS (Áo)",
+    text: "Đại học Waikato (New Zealand)",
+    marginTop: 2,
+  },
+];
+
+export var adviseItemList = [
+  {
+    title: "bắc mỹ",
+    desc: "Bạn sẽ được du học tại Canada, UK,...",
+    haveBorder: true,
+  },
+  {
+    title: "bắc mỹ",
+    desc: "Bạn sẽ được du học tại Canada, UK,...",
+    haveBorder: true,
+  },
+  {
+    title: "bắc mỹ",
+    desc: "Bạn sẽ được du học tại Canada, UK,...",
+    haveBorder: false,
+  },
+  {
+    title: "bắc mỹ",
+    desc: "Bạn sẽ được du học tại Canada, UK,...",
+    haveBorder: false,
+  },
+];
+
+export var footerContentLeftList = [
+  {
+    text: "Địa chỉ: Km 9 Nguyễn Trãi, Thanh Xuân, Hà Nội, Việt Nam",
+    href: "#",
+  },
+  {
+    text: "Điện thoại: 084123456",
+    href: "tel:084123456",
+  },
+  {
+    text: "Hotline: Ms Nguyet 091234567 , Ms Huyen 091234567",
+    href: "tel:091234567",
+  },
+  {
+    text: "(Fax): 09123456",
+    href: "fax:09123456",
+  },
+  {
+    text: "Email: iec@hanu.edu.vn",
+    href: "mailto:iec@hanu.edu.vn",
+  },
+];
+
+export var footerContentRightList = [
+  { text: "Chương trình đào tạo", href: "#" },
+  { text: "Nghiên cứu khoa học", href: "#" },
+  { text: "Liên hệ", href: "#" },
+  { text: "Tài nguyên", href: "#" },
+  { text: "Sinh viên IEC", href: "#" },
+  { text: "F&A", href: "#" },
+];
+
+export var footerSocialList = [
+  { icon: "fa-brands fa-facebook", href: "#" },
+  { icon: "fa-regular fa-envelope", href: "#" },
+  { icon: "fa-brands fa-linkedin", href: "#" },
+];
+
+export var ABOUTME_LIST_IMAGE = [
+  {
+    image: "./image/aboutme1.jpg",
+    alt: "aboutme1",
+  },
+  {
+    image: "./image/aboutme2.jpg",
+    alt: "aboutme2",
+  },
+  {
+    image: "./image/aboutme3.jpg",
+    alt: "aboutme3",
+  },
+  {
+    image: "./image/aboutme4.jpg",
+    alt: "aboutme4",
+  },
+  {
+    image: "./image/aboutme5.jpg",
+    alt: "aboutme5",
+  },
+  {
+    image: "./image/aboutme6.jpg",
+    alt: "aboutme6",
+  },
+];
+
+export var iconCompanyList = [
+  { img: "./imgs/iconCompany1.png", alt: "logo1" },
+  { img: "./imgs/iconCompany2.png", alt: "logo2" },
+  { img: "./imgs/iconCompany3.png", alt: "logo3" },
+  { img: "./imgs/iconCompany4.png", alt: "logo4" },
+  { img: "./imgs/iconCompany5.png", alt: "logo5" },
+  { img: "./imgs/iconCompany6.png", alt: "logo6" },
+  { img: "./imgs/iconCompany7.png", alt: "logo7" },
+];
---
+++ js/main.js
@@ -0,0 +1,152 @@
+import {
+  navMenuList,
+  programItemList,
+  adviseItemList,
+  footerContentLeftList,
+  footerContentRightList,
+  footerSocialList,
+  iconCompanyList,
+} from "./constants.js";
+
+var navMenu = document.getElementById("navMenu");
+var programItemContainer = document.getElementById("programItemContainer");
+var adviseItemContainer = document.getElementById("adviseItemContainer");
+var footerContentLeft = document.getElementById("footerContentLeft");
+var footerContentRight = document.getElementById("footerContentRight");
+var footerSocialContainer = document.getElementById("footerSocial");
+var companyLogoContainer = document.getElementById("companyLogoContainer");
+
+companyLogoContainer.innerHTML = iconCompanyList
+  .map((item) => {
+    return `
+  <img src="${item.img}" alt="${item.alt}" class="w150"/>
+  `;
+  })
+  .join("");
+
+navMenu.innerHTML = navMenuList
+  .map((item) => {
+    return `
+  <div><a href="${item.href}" target="_blank" rel="noopener noreferrer" class="ttu cf fwb">${item.text}</a></div>
+  `;
+  })
+  .join("");
+
+programItemContainer.innerHTML = programItemList
+  .map((item) => {
+    return `
+    <div
+      class="brtr50 bóng brtl50 brbl10 brbr10 w33 df jcc fdc pl25 pr25 pb25 pt20 pt40 hfc ${
+        item.marginTop === 0 ? `mt0` : item.marginTop === 1 ? `mt250` : "mt500"
+      }"
+    >
+      <img
+        class="w1 mla mra mb50 h250"
+        src="${item.img}"
+        alt="program1"
+      />
+      <div class="grayBoldColor fwb fs15 mb25">
+       ${item.title}
+      </div>
+      <div class="tal">${item.text1}</div>
+      <div class="tal">${item.text2}</div>
+    </div>
+    `;
+  })
+  .join("");
+
+adviseItemContainer.innerHTML = adviseItemList
+  .map((item) => {
+    return `
+    <div class="w40 ${item.haveBorder && "bb1 bc0 bss pb25 mb50"}">
+    <div class="fs2 fwb ttc mb15">${item.title}</div>
+    <div class="fs13 fwn">${item.desc}</div>
+  </div>
+    `;
+  })
+  .join("");
+
+footerContentLeft.innerHTML = footerContentLeftList
+  .map((item) => {
+    return `
+      <a class="mb10" href="${item.href}">${item.text}</a>
+    `;
+  })
+  .join("");
+
+footerContentRight.innerHTML = footerContentRightList
+  .map((item) => {
+    return `
+    <a class="mb25 w49" href="${item.href}">${item.text}</a>
+    `;
+  })
+  .join("");
+
+footerSocialContainer.innerHTML = footerSocialList
+  .map((item) => {
+    return `  
+    <a class="" href="${item.href}"><i class="${item.icon} fs2 mr25"></i></a>
+
+    `;
+  })
+  .join("");
+
+document.addEventListener("DOMContentLoaded", function () {
+  var slidesContainer = document.querySelector(".slides-container-aboutMe");
+  var prevButton = document.querySelector(".prev-aboutMe");
+  var nextButton = document.querySelector(".next-aboutMe");
+  var currentIndex = 0;
+
+  // ABOUTME_LIST_IMAGE.forEach(function (item) {
+  //   var slide = document.createElement("div");
+  //   var img = document.createElement("img");
+
+  //   slide.className = "slide";
+  //   img.src = item.image;
+  //   img.alt = item.alt;
+
+  //   slide.appendChild(img);
+  //   slidesContainer.appendChild(slide);
+  // });
+
+  var slides = document.querySelectorAll(".slide");
+
+  var firstSlide = slides[0].cloneNode(true);
+  var lastSlide = slides[slides.length - 1].cloneNode(true);
+
+  slidesContainer.appendChild(firstSlide);
+  slidesContainer.insertBefore(lastSlide, slides[0]);
+
+  slides = document.querySelectorAll(".slide");
+
+  slidesContainer.style.transform = `translateX(-100%)`;
+
+  function changeSlide(step) {
+    currentIndex += step;
+
+    slidesContainer.style.transition = "transform 0.5s ease";
+    slidesContainer.style.transform = `translateX(${
+      -(currentIndex + 1) * 100
+    }%)`;
+
+    if (currentIndex < 0) {
+      setTimeout(function () {
+        slidesContainer.style.transition = "none";
+        currentIndex = ABOUTME_LIST_IMAGE.length - 1;
+        slidesContainer.style.transform = `translateX(${
+          -(currentIndex + 1) * 100
+        }%)`;
+      }, 500);
+    } else if (currentIndex >= ABOUTME_LIST_IMAGE.length) {
+      setTimeout(function () {
+        slidesContainer.style.transition = "none";
+        currentIndex = 0;
+        slidesContainer.style.transform = `translateX(-100%)`;
+      }, 500);
+    }
+  }
+
+  setInterval(function () {
+    changeSlide(1);
+  }, 3000);
+});
